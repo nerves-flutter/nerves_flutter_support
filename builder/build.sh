58b9300ecdfb285590d31a21a9042fef5af01c3d
Digit <aaron.shea@smartrent.com>
2025-08-13 10:46:58 -0700

    Include libfreetype in runtime artifacts archive

--- builder/build.sh
+++ builder/build.sh
@@ -49,6 +49,7 @@ cp o/$base/target/usr/lib/libuv.so* ../priv/lib
 cp o/$base/target/usr/lib/libxkbcommon.so* ../priv/lib
 cp o/$base/target/usr/lib/libkmod.so* ../priv/lib
 cp o/$base/target/usr/lib/libfontconfig.so* ../priv/lib
+cp o/$base/target/usr/lib/libfreetype.so* ../priv/lib
 
 # eudev + udev rules
 cp o/$base/target/usr/bin/udevadm ../priv/bin
